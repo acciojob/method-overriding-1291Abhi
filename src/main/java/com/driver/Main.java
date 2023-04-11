1851503b25527d712d99685960b2a00029b42835
Abhinesh Singh <43317384+1291Abhi@users.noreply.github.com>
2023-04-11 12:59:16 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+ class A {
+   static  String meth(){
+        return  "Invoking method from class A";
+    }
+}
+class B extends A{
+
+}
 public class Main {
+  public static void main(String[] args){
+    B ex=new B();
+    System.out.println(ex.meth());
+  }
   
 }
