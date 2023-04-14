de096ac0b4f3144b35ecd1eecf78253309a807c1
Abhinesh Singh <abhisingh1291@gmail.com>
2023-04-14 14:58:33 +0530

    bug fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,10 @@
 package com.driver;
 
- class A {
-    String meth(){
-        return  "Invoking method from class A";
-    }
-}
-class B extends A{
-  @Override
-    String meth(){
-        return  "Method is overridden in Extendend class B";
-    }
 
-}
 public class Main {
   public static void main(String[] args){
-    B ex=new B();
-    String ans= ex.meth();
+    B b=new B();
+    b.meth();
   }
   
 }
