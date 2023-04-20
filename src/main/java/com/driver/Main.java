7eb4c224ce6f7208b9a709e1b271755cbf533244
Abhinesh Singh <abhisingh1291@gmail.com>
2023-04-20 13:34:01 +0530

    fixing error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,21 @@ package com.driver;
 
 
 public class Main {
+  static class A{
+    String meth(){
+      return "Invoking method from class A";
+    }
+  }
+  static class B extends A{
+    @Override
+    String meth(){
+     return  "Method is overridden in Extendend class B";
+    }
+  }
   public static void main(String[] args){
     B b=new B();
     A a=new A();
-    System.out.println(a.meth());
+//    System.out.println(a.meth());
     System.out.println(b.meth());
   }
   
